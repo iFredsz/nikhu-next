c2df6016ea79733a22de32ce7a67ad8f416c36cf
iFredsz <ferdydeva@gmail.com>
2025-10-12 16:01:35 +0700

    Update 12 Oktober V2

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  initializeFirestore,
+} from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-// import { getAnalytics } from 'firebase/analytics'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
@@ -21,9 +18,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
-const db = getFirestore(app)
 
-// const analytics = getAnalytics(app)
+// Initialize Firestore with settings to handle long polling
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+  experimentalAutoDetectLongPolling: true,
+})
+
+const auth = getAuth(app)
 
 export { app, auth, db }
